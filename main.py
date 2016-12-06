78b9c1cbb70129d19333529b51f1858775406339
avi <hi@avi.im>
2016-12-06 22:47:01 +0530

    Use peewee for persistence and check images are posted earlier

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telegram
 import praw
+from peewee import SqliteDatabase, Model, CharField, IntegrityError
 
 from settings import (TELEGRAM_ACCESS_TOKEN, TELEGRAM_CHANNEL_ID,
                       REDDIT_APP_KEY, REDDIT_APP_SECRET, REDDIT_USER_AGENT)
@@ -8,6 +9,32 @@ bot = telegram.Bot(token=TELEGRAM_ACCESS_TOKEN)
 reddit_client = praw.Reddit(user_agent=REDDIT_USER_AGENT,
                             client_id=REDDIT_APP_KEY,
                             client_secret=REDDIT_APP_SECRET)
+db = SqliteDatabase('wutong.db')
+
+
+class FetchedThreads(Model):
+    thread_id = CharField(unique=True)
+
+    class Meta:
+        database = db
+
+
+def initialize_db():
+    db.connect()
+    # Create table only if it doesn't exist
+    db.create_tables([FetchedThreads, ], safe=True)
+
+
+def deinit():
+    db.close()
+
+
+def mark_thread_posted(thread_id):
+    FetchedThreads.create(thread_id=thread_id)
+
+
+def is_thread_posted(thread_id):
+    return FetchedThreads.select().where(FetchedThreads.thread_id == thread_id)
 
 
 def post_image_to_tg(image_url, caption=''):
@@ -16,11 +43,15 @@ def post_image_to_tg(image_url, caption=''):
                    caption=caption[:200])
 
 
-def get_stuff_from_reddit(subreddit='gentlemanboners'):
+def post_stuff_from_reddit(subreddit='gentlemanboners'):
     for submission in reddit_client.subreddit(subreddit).new():
-        if submission.post_hint == 'image':
-            post_image_to_tg(image_url=submission.url,
-                             caption=submission.title)
+        if not submission.post_hint == 'image':
+            continue
+        thread_id = submission.id
+        if is_thread_posted(thread_id=thread_id):
+            break
+        post_image_to_tg(image_url=submission.url, caption=submission.title)
+        mark_thread_posted(thread_id=thread_id)
 
 
 def main():
@@ -28,4 +59,6 @@ def main():
 
 
 if __name__ == '__main__':
-    get_stuff_from_reddit()
+    initialize_db()
+    post_stuff_from_reddit()
+    deinit()
