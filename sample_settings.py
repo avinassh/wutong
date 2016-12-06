821fd9cb8226726aa22711b550b89c19a73afe4b
avi <hi@avi.im>
2016-12-06 23:00:17 +0530

    Add sample settings

---
+++ sample_settings.py
@@ -0,0 +1,6 @@
+TELEGRAM_ACCESS_TOKEN = ''
+TELEGRAM_CHANNEL_ID = '@gentlemanboners'
+REDDIT_APP_KEY = ''
+REDDIT_APP_SECRET = ''
+REDDIT_USER_AGENT = ('Wutong Bot, v0.1. Read only bot to read posts from'
+                     '/r/gentlemanboners (by /u/avinassh)')
